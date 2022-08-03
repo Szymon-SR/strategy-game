a790c0e6b48ff188782362021093e52e20b7614d
Szymon-SR <srszymonsr@gmail.com>
2022-08-03 23:57:46 +0200

    constant width map

--- frontend/connect4.js
+++ frontend/connect4.js
@@ -7,15 +7,15 @@ function createBoard(board) {
   
   // Generate hex map.
 
-  const columnElement = document.createElement("div");
-  columnElement.className = "column";
-  board.append(columnElement);
-  //columnElement.dataset.column = column;
-  for (let hexNumber = 0; hexNumber < 102; hexNumber++) {
-    const cellElement = document.createElement("div");
-    cellElement.className = "hex";
-    cellElement.dataset.hexId = hexNumber;
-    columnElement.append(cellElement);
+  const mapElement = document.createElement("div");
+  mapElement.className = "map";
+  board.append(mapElement);
+
+  for (let hexNumber = 0; hexNumber < 64; hexNumber++) {
+    const hexElement = document.createElement("div");
+    hexElement.className = "hex";
+    hexElement.dataset.hexId = hexNumber;
+    mapElement.append(hexElement);
   }
 }
 
