ccd03229903228cb86a48bc89b37a381c385fb5e
Szymon-SR <srszymonsr@gmail.com>
2022-08-11 01:42:27 +0200

    working two player async io

--- backend/app.py
+++ backend/app.py
@@ -2,56 +2,134 @@ import asyncio
 import json
 import websockets
 import random
+import secrets
 
 from game_state import Game
 
 import logging
 logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
-# set of all connected clients
-websocket_clients = set()
-
+# set of all games
 JOIN = {}
 
-async def handle_connection(websocket, game: Game, player: int):
+
+async def handle_incoming(websocket, game: Game, player: int):
     # IN
+    async for message in websocket:
+        event = json.loads(message)
+        print(event)
+        # TODO player's move affect game obj
 
-    websocket_clients.add(websocket)
-    print(f'New connection from: {websocket.remote_address} ({len(websocket_clients)} total)')
 
-    try:
-        async for message in websocket:
-            event = json.loads(message)
-            print(event)
-            # TODO player's move affect game
-
-    except websockets.exceptions.ConnectionClosedError as cce:
-        print("Connection closed")
-    finally:
-        websocket_clients.remove(websocket)
-
-async def send_game_state():
+async def send_game_state(game: Game, connected):
     # OUT
     """Send current game state to all connected clients"""
     while True:
+        game.run_tick()
         # TODO replace with game state JSON
-        num = str(random.randint(10, 99))
-        game_state = 
+        game_state = {
+            "type": "game_state",
+            "day": game.day
+        }
 
-        for c in websocket_clients:
-            print(f'Sending [{num}] to socket [{id(c)}]')
-            await c.send(num)
-        await asyncio.sleep(2)
+        websockets.broadcast(connected, json.dumps(game_state))
 
-async def serve():
-    async with websockets.serve(handle_connection, "", 8001):
-        await asyncio.Future()  # run forever
+        await asyncio.sleep(1)  # TODO adjust update time
+
+
+async def start(websocket):
+    """
+    Handle a connection from the first player: start a new game.
+    """
+
+    game = Game()
+
+    connected = {websocket}
+    join_key = secrets.token_urlsafe(12)
+    JOIN[join_key] = game, connected
+
+    try:
+        # Send the secret access tokens to the browser of the first player,
+        # where they'll be used for building "join" and "watch" links.
+
+        event = {
+            "type": "init",
+            "join": join_key,
+        }
+
+        await websocket.send(json.dumps(event))
+        # Receive and process moves from the first player.
+        await handle_incoming(websocket, game, 0)
+
+    finally:
+        del JOIN[join_key]
+
+
+async def join(websocket, join_key):
+    """
+    Handle a connection from the second player: join an existing game.
+    """
+
+    # Find the Connect Four game.
+    try:
+        game, connected = JOIN[join_key]
+
+    except KeyError:
+        await error(websocket, "Game not found.")
+        return
+
+    # Register to receive moves from this game.
+    connected.add(websocket)
+
+    try:
+        # Receive and process moves from the second player, also start sending game state
+        tasks = []
+        # TODO not one but the amount of players
+        tasks.append(asyncio.create_task(handle_incoming(websocket, game, 1)))
+        tasks.append(asyncio.create_task(send_game_state(game, connected)))
+        await asyncio.gather(*tasks)
+
+    finally:
+        connected.remove(websocket)
+
+
+async def error(websocket, message):
+    """
+    Send an error message.
+    """
+
+    event = {
+        "type": "error",
+        "message": message,
+    }
+
+    await websocket.send(json.dumps(event))
+
+
+async def handler(websocket):
+    """
+    Handle a connection and dispatch it according to who is connecting.
+    """
+
+    # Receive and parse the "init" event from the UI.
+    message = await websocket.recv()
+    event = json.loads(message)
+    print("Got event")
+    print(event)
+    assert event["type"] == "init"
+
+    if "join" in event:
+        # Second player joins an existing game.
+        await join(websocket, event["join"])
+    else:
+        # First player starts a new game.
+        await start(websocket)
+
 
 async def main():
-    tasks = []
-    tasks.append(asyncio.create_task(serve()))
-    tasks.append(asyncio.create_task(send_game_state()))
-    await asyncio.gather(*tasks)
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     # creates an asyncio event loop, runs the main()
--- frontend/main.js
+++ frontend/main.js
@@ -1,28 +1,18 @@
 import { createBoard, visualizeAttack } from "./game.js";
+console.log("Hejo")
 
-window.addEventListener("DOMContentLoaded", () => {
-  // Initialize the UI.
-  const board = document.querySelector(".board");
-  createBoard(board);
-  // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket("ws://localhost:8001/");
-  receiveGameEvents(board, websocket);
-  sendAttacks(board, websocket);
-});
-
-
-function sendAttacks(board, websocket) {
-  // When clicking a hex, send an event
-  board.addEventListener("click", ({ target }) => {
-    const id = target.dataset.hexId;
-    // Ignore clicks outside a column.
-    if (id === undefined) {
-      return;
+function initGame(websocket) {
+  websocket.addEventListener("open", () => {
+    // Send an "init" event according to who is connecting.
+    const params = new URLSearchParams(window.location.search);
+    let event = { type: "init" };
+    if (params.has("join")) {
+      // Second player joins an existing game.
+      event.join = params.get("join");
+    } 
+    else {
+      // First player starts a new game.
     }
-    const event = {
-      type: "attack",
-      id: parseInt(id, 10),
-    };
     websocket.send(JSON.stringify(event));
   });
 }
@@ -43,6 +33,13 @@ function receiveGameEvents(board, websocket) {
     // event will look like: event = {"type": "attack", "player": "red", "hexId": 3, "success": 1}
     const event = JSON.parse(data);
     switch (event.type) {
+      case "init":
+        // Create links for inviting the second player
+        document.querySelector(".room").innerHTML = "http://0.0.0.0:8000/?join=" + event.join;
+        break;
+      case "game_state":
+        updateGame(event)
+        break;
       case "attack":
         visualizeAttack(board, event.player, event.hexId, event.success)
         break;
@@ -59,3 +56,40 @@ function receiveGameEvents(board, websocket) {
     }
   });
 }
+
+function updateGame(gameData) {
+  document.getElementById("value days").innerHTML = gameData.day;
+}
+
+function sendAttacks(board, websocket) {
+  // When clicking a hex, send an event
+  board.addEventListener("click", ({ target }) => {
+    const id = target.dataset.hexId;
+    // Ignore clicks outside a column.
+    if (id === undefined) {
+      return;
+    }
+    console.info("Hejo2")
+    const event = {
+      type: "attack",
+      id: parseInt(id, 10),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Initialize the UI.
+  const board = document.querySelector(".board");
+  createBoard(board);
+  // Open the WebSocket connection and register event handlers.
+  const websocket = new WebSocket("ws://localhost:8001/");
+  initGame(websocket);
+  receiveGameEvents(board, websocket);
+  sendAttacks(board, websocket);
+});
+
+
+
+
+
