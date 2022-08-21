3b3ddfc0ea1f4129b474c601c84128346425cff9
Szymon-SR <srszymonsr@gmail.com>
2022-08-21 23:31:23 +0200

    connected new frontend to backend

--- newreact/src/index.js
+++ newreact/src/index.js
@@ -1,29 +1,93 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { CenterPanel } from "./board-components.js";
 
+function initGame(websocket) {
+  websocket.addEventListener("open", () => {
+
+    // Send an "init" event according to who is connecting.
+    const params = new URLSearchParams(window.location.search);
+    let event = { type: "init" };
+    if (params.has("join")) {
+      // Second player joins an existing game.
+      event.join = params.get("join");
+    } 
+    else {
+      // First player starts a new game.
+    }
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+
+// Components 
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    // TODO change to real
-    this.gameState = {
+
+    this.state = {
+      playerIndex: 0,
       day: 69,
       home_tiles: [11, 21], // home tiles of players, by player index
       money_balances: [420, 240],
       incomes: [0.5, 1.5],
       owned_tiles: [[11, 12, 13], [21, 22, 23]],
-  }
-  playerIndex = 
+    }
 
+    this.numberOfTiles = 64;  // how many tiles are on the map
+  }
+
+  receiveGameEvents(websocket) {
+    websocket.addEventListener("message", ({ data }) => {
+      const event = JSON.parse(data);
+      switch (event.type) {
+        case "init":
+          // Create links for inviting the second player
+          const link = "http://0.0.0.0:3000/?join=" + event.join;
+          this.setState({inviteLink: link});
+          
+          // Getting init message means that this is the first player
+          // playerIndex = 0
+          break;
+        case "game_state":
+          this.setState(event);
+          break;
+        case "index_assignment":
+          this.setState({playerIndex: event.index});
+          break;
+        case "error":
+          console.error(event.message);
+          break;
+        default:
+          throw new Error(`Unsupported event type: ${event.type}.`);
+      }
+    });
+  }
+
+  componentDidMount() {
+    // runs after output has been rendered
+    const websocket = new WebSocket("ws://localhost:8001/");
+    initGame(websocket);
+    this.receiveGameEvents(websocket);
   }
 
   render() {
-
-
     return (
       <div className="game">
-        <BaseMenu  />
-        <CenterPanel />
+        <BaseMenu
+          playerIndex = {this.state.playerIndex}
+          inviteLink = {this.state.inviteLink}
+          />
+        <CenterPanel 
+          day = {this.state.day}
+          home_tiles = {this.state.home_tiles}
+          // balance and income are personalized for specific player
+          balance = {this.state.money_balances[this.state.playerIndex]}
+          income = {this.state.incomes[this.state.playerIndex]}
+          owned_tiles = {this.state.owned_tiles}
+          numberOfTiles = {this.numberOfTiles}
+        />
         <FocusMenu />
       </div>
     );
@@ -33,35 +97,51 @@ class Game extends React.Component {
 function BaseMenu(props) {
   return (
     <div
-      className="basemenu"
+      className="base-menu"
     >
-      <PlayerInfo />
-      <RoomInfo />
+      <PlayerInfo playerIndex = {props.playerIndex} />
+      <RoomInfo inviteLink = {props.inviteLink} />
       <ScoreInfo />
     </div>
   )
 }
 
-
-function Hex(props) {
-  const [hexId, setHexId] = useState(props.hexNumber);
-  const [ownerId, setOwnerID] = useState(0);
-
+function PlayerInfo(props) {
   return (
-    <div
-      className={`hex neutral`}
-      onClick={props.onClick} // this function can be passed if we want to change parent
-    >
+    <div>
+      Your id: {props.playerIndex}
     </div>
-  );
+  )
 }
 
-//   for (let hexNumber = 0; hexNumber < 64; hexNumber++) {
-//     const hexElement = document.createElement("div");
-//     hexElement.className = "hex neutral";
-//     hexElement.dataset.hexId = hexNumber;
-//     mapElement.append(hexElement);
-//   }
+function RoomInfo(props) {
+  return (
+    <div>
+      {props.inviteLink}
+    </div>
+  )
+}
+
+function ScoreInfo(props) {
+  return (
+    <div>
+      Not yet
+    </div>
+  )
+}
+
+//Right
+function FocusMenu(props) {
+  return (
+    <div
+      className="focus-menu"
+    >
+      Not yet
+    </div>
+  )
+}
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
